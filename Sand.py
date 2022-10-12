6add19834098f2b3a2e425be2f13eece54382657
williamjones7 <s2444261@ed.ac.uk>
2022-10-13 00:12:07 +0100

    Commenting changed

--- Sand.py
+++ Sand.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation
-%matplotlib inline
 
 #0 dirichlet B.C's - no sand at either end of the belt
 dir0 = 0.0  # u(0,t)= dir0
 dir1 = 0.0  # u(1,t)= dir1
 
-#  function for setting initial conditions in space
+#Function for setting initial conditions in space
 def I(x): # initial u(x,0) = 0, dump sand everywhere
     len_x = np.size(x)
     i_x = np.zeros(len_x)
@@ -29,7 +28,6 @@ def I(x): # initial u(x,0) = 0, dump sand everywhere
 #     u_ex[M-1] = dir1 # dirichlet boundary condition at u(L,t)
 #     return u_ex
 
-#plot function
 def plotting(U,L):
     size_t = np.size(U,1)-1 # number of time steps
    
@@ -42,25 +40,22 @@ def plotting(U,L):
     N_dots = 20
     x_dots = np.linspace(0, L, N_dots+1)    # spacial points to plot exact solution at
 
-# some arbitarily chosen time points to compare at
+    #some arbitarily chosen time points to compare at
     for plot_pos in [0, int(size_t/10),int(size_t/5), int(size_t/2), int(size_t)]:
     
-    # exact solution
+    #exact solution
         # U_tplot = U_exact(x_dots,t[plot_pos]) 
         # label = "Exact, t=" + "%0.3f" % (t[plot_pos],)
         # ax.plot(x_dots,U_tplot,linestyle = ':',color = colours[colour_pos],marker = markers[0], label=label)
 
-    # numerical solution
+    #numerical solution
         label = "Numerical PDE, t=" + "%0.3f" % (t[plot_pos],)
         ax.plot(x_pde,U[:,plot_pos],color = colours[colour_pos],ls='--', label=label)
         colour_pos = colour_pos + 1;
     
-
     plt.xlim(0,L) # zoom in on area of interest
     ax.legend() # turn on legend 
     plt.show()
-
-
 
 ## ====== setting parameters =======
 
@@ -71,26 +66,25 @@ T = 1           # final time
 Nt_points = Nt_gaps + 1
 
 t = np.linspace(0.,T,Nt_points)  # times at each time step
-Nx_spaces = 100; # number of spaces in x direction
+Nx_spaces = 100; #number of spaces in x direction
 Nx_points = Nx_spaces + 1 
 L = 1; 
-x_pde = np.linspace(0, L, Nx_points)    # mesh points in space
+x_pde = np.linspace(0, L, Nx_points)    #mesh points in space
 dx = x_pde[1] - x_pde[0] 
 dt = t[1] - t[0]
 C = dt/(dx**2)
 print("Delta x =", dx, "Delta t = ", dt, "C =", C)
 
-
 ##----------intialise lists ------------------------
 
-# Define the numerical solution 
-# the first index is space and the second time
+#Define the numerical solution 
+#The first index is space and the second time
 U = np.zeros((Nx_points,Nt_points))
 
-# The initial condition
-
+#The initial condition
 U[:,0]=I(x_pde)
-# enforcing the boundary condition
+
+#Boundary conditions
 U[0,0]  = dir0 
 U[-1,0] = dir1
 
@@ -104,18 +98,18 @@ U[:,0] = u_old
 #compute numerical solution 
 for n in range(1, Nt_points):
     
-    # set Dirichlet boundary points here
+    #set Dirichlet boundary points here
     u[0] = dir0
     u[-1] = dir1
     
-    # compute u at inner mesh points
+    #compute u at inner mesh points
     for i in range(1, Nx_points-1):
         u[i] = u_old[i] + (dt/(dx**2))*(u[i-1]-2*u[i]+u[i+1])
  
-    # update u_old before next step
+    #update u_old before next step
     u_old[:]= u
 
-    # copy into full storage
+    #copy into full storage
     U[:,n] = u;
     
 plotting(U, L)
