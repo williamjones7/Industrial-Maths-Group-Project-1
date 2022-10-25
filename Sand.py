6e9adac0c8fa8318f5a2d78460dea49edcb8ee0a
williamjones7 <s2444261@ed.ac.uk>
2022-10-25 16:57:50 +0100

    Open to Jack to play with

--- Sand.py
+++ Sand.py
@@ -5,7 +5,7 @@ import matplotlib.animation
 ## ====== setting parameters =======
 
 Nt_gaps = 30000    # number of timesteps
-T = 30           # final time - we're interested in time as it gets large  
+T = 30          # final time - we're interested in time as it gets large  
 Nt_points = Nt_gaps + 1
 
 t = np.linspace(0.,T,Nt_points)  # times at each time step
@@ -17,7 +17,7 @@ dir0 = 0.0  # u(0,t)= dir0
 dir1 = 0.0  # u(L,t)= dir1
 
 length = 5 #default conveyor length
-# #Set up spacial points for exact solution
+#Set up spacial points for exact solution
 N_dots = 50
 
 ## ===== defining our functions =====
@@ -88,7 +88,8 @@ def numerical(S, V, D, L):
         
         #compute u at inner mesh points
         for i in range(1, Nx_points-1):
-            u[i] = (p/2 + r)*u_old[i-1] + (1 - 2*r)*u_old[i] + (r - p/2)*u_old[i+1] + S(u_old[i], n)*dt
+            xpos = L*i/Nx_points
+            u[i] = (p/2 + r)*u_old[i-1] + (1 - 2*r)*u_old[i] + (r - p/2)*u_old[i+1] + S(xpos, n)*dt
     
         #update u_old before next step
         u_old[:]= u.copy()
@@ -103,7 +104,7 @@ def numerical(S, V, D, L):
 
 markers =['x', '+', '1', 'o']
 linestyle = ['solid', 'dotted', 'dashed', 'dashdot', 'solid']
-colours = ['maroon', 'red', 'lightcoral', 'green', 'mediumseagreen', 'palegreen', 'navy', 'blue', 'cornflowerblue'] # make comparison easy
+colours = ['red', 'green', 'blue', 'orange', 'yellow', 'purple'] # make comparison easy
 reds = ['darkred', 'crimson', 'indianred', 'red', 'lightsalmon', 'salmon']
 greens =['darkgreen', 'green', 'seagreen', 'mediumseagreen','springgreen', 'palegreen']
 blues = ['navy', 'royalblue','mediumslateblue', 'dodgerblue', 'skyblue', 'lightsteelblue']
@@ -167,38 +168,86 @@ blues = ['navy', 'royalblue','mediumslateblue', 'dodgerblue', 'skyblue', 'lights
 # plt.ylabel('Height, H', fontsize = 15)
 # plt.savefig('Length Figure 1.png')
 
+# plt.figure(3_2, figsize=(10, 8), dpi=200)
+# #define lengths
+# lengths = [5,6,7,8,9,10]
+# for i in range(0, len(lengths)):
+#     label = "Exact, L=" + "%0.3f" % (lengths[i])
+#     x_dots, u_exact = exact(5/lengths[i], 0.25, 0.5, lengths[i])
+#     plt.plot(x_dots, u_exact,color = blues[i], linestyle = 'dashed', marker = markers[0], label = label)
+
+# plt.legend()
+# plt.yticks([])
+# plt.xlim(0,max(lengths)) # zoom in on area of interest
+# plt.xlabel('Spacial Position, x', fontsize = 15)
+# plt.ylabel('Height, H', fontsize = 15)
+# plt.savefig('Length Figure 2.png')
+
 # ===== Max height =====
 
-plt.figure(4, figsize=(10,8), dpi =80)
-Nintervals = 50
+# plt.figure(4, figsize=(10,8), dpi =80)
+# Nintervals = 50
 
-max_height_adv = []
-interval_size = (0.5-0.25)/Nintervals
-for v in np.arange(0.25, 0.25 + interval_size*(Nintervals+1), interval_size):
-    max_height_adv.append(max(exact(1.0, v, 0.5, length)[-1]))
-plt.plot(max_height_adv, label = 'Advection, V', color = 'red')
+# max_height_adv = []
+# interval_size = (0.5-0.25)/Nintervals
+# for v in np.arange(0.25, 0.25 + interval_size*(Nintervals+1), interval_size):
+#     max_height_adv.append(max(exact(1.0, v, 0.5, length)[-1]))
+# plt.plot(max_height_adv, label = 'Advection, V', color = 'red')
 
-interval_size = (1.0-0.5)/Nintervals
-max_height_diff = []
-for d in np.arange(0.5, 0.5 + interval_size*(Nintervals+1), interval_size):
-    max_height_diff.append(max(exact(1.0, 0.25, d, length)[-1]))
-plt.plot(max_height_diff, label = 'Diffusion, D', color = 'green')
+# interval_size = (1.0-0.5)/Nintervals
+# max_height_diff = []
+# for d in np.arange(0.5, 0.5 + interval_size*(Nintervals+1), interval_size):
+#     max_height_diff.append(max(exact(1.0, 0.25, d, length)[-1]))
+# plt.plot(max_height_diff, label = 'Diffusion, D', color = 'green')
 
-interval_size = (10-5)/Nintervals
-max_height_length = []
-for l in np.arange(5, 5  + interval_size*(Nintervals+1), interval_size):
-    max_height_length.append(max(exact(1.0, 0.25, 0.5, l)[-1]))
-plt.plot(max_height_length, label = 'Length, L', color = 'blue')
+# interval_size = (10-5)/Nintervals
+# max_height_length = []
+# for l in np.arange(5, 5  + interval_size*(Nintervals+1), interval_size):
+#     max_height_length.append(max(exact(1.0, 0.25, 0.5, l)[-1]))
+# plt.plot(max_height_length, label = 'Length, L', color = 'blue')
 
-plt.plot(max(exact(1.0, 0.25, 0.5, length)[-1])*np.ones(Nintervals), linestyle = 'dashed', color='Black', label = 'D = 0.5, V = 0.25, L = 5')
+# plt.plot(max(exact(1.0, 0.25, 0.5, length)[-1])*np.ones(Nintervals), linestyle = 'dashed', color='Black', label = 'D = 0.5, V = 0.25, L = 5')
+# plt.legend()
+# plt.yticks([])
+# plt.xticks([])
+# plt.xlabel('Change in coefficient', fontsize = 16)
+# plt.ylabel('Max height of sand pile', fontsize = 16)
+# plt.savefig('Figure Height.png')
+
+# ===== S(x, t) non-constant, fix length as 5, D = 0.5, V = 0.5=====
+
+def S_middle_metre(x, t):
+    if 2 < x < 3:
+        return 5.0
+    else:
+        return 0.0
+
+def S_control(x, t):
+    if x < length:
+        return 1.0
+    else:
+        return 0.0
+
+plt.figure(6, figsize=(10, 8), dpi=200)
+#Find sand steady state for dumping at first metre
+label = "Dump sand at middle metre"
+x_pde, u_num = numerical(S_middle_metre, 0.5, 0.2, 5)
+plt.plot(x_pde, u_num[:,-1], color = colours[0], linestyle = 'dashed', label = label)
+
+#Find sand steady state for dumping at first metre
+label = "Dump sand everywhere control"
+x_pde, u_num = numerical(S_constant, 0.5, 0.2, 5)
+plt.plot(x_pde, u_num[:,-1], color = colours[1], linestyle = 'dashed', label = label)
+
+plt.figure(6, figsize=(10, 8), dpi=200)
+#Find sand steady state for dumping at first metre
+label = "Control"
+x_pde, u_num = numerical(S_control, 0.5, 0.2, 5)
+plt.plot(x_pde, u_num[:,-1], color = colours[2], linestyle = '', marker = markers[0], label = label)
+
 plt.legend()
 plt.yticks([])
-plt.xticks([])
-plt.xlabel('Change in coefficient', fontsize = 16)
-plt.ylabel('Max height of sand pile', fontsize = 16)
-plt.savefig('Figure Height.png')
-
-
-# ===== S(x, t) non-constant =====
-
-
+plt.xlim(0,length) # zoom in on area of interest
+plt.xlabel('Spacial Position, x', fontsize = 15)
+plt.ylabel('Height, H', fontsize = 15)
+plt.savefig('Sand at start.png')
